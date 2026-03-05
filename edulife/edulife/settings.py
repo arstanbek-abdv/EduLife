00857ca003b57f4e39a3303719f5ccb414285ede
Arstanbek <abdyldaevarstanbek79@gmail.com>
2026-03-05 13:05:52 +0600

    set DEBUG=False and added render allowed host

--- edulife/edulife/settings.py
+++ edulife/edulife/settings.py
@@ -25,9 +25,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv("DJANGO_SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.getenv("DJANGO_DEBUG", "true").lower() in ("1", "true", "yes")
+DEBUG = os.getenv("DJANGO_DEBUG", "false").lower() in ("1", "true", "yes")
 
-ALLOWED_HOSTS = [h.strip() for h in os.getenv("DJANGO_ALLOWED_HOSTS", "localhost,127.0.0.1").split(",")]
+ALLOWED_HOSTS = [h.strip() for h in os.getenv("DJANGO_ALLOWED_HOSTS", 'edulife-1-gxh2.onrender.com').split(",")]
 
 # CORS: с каких origin разрешены запросы к API (фронт на другом порту/домене)
 # Через запятую, без пробелов после запятой: http://localhost:5173,https://my-app.vercel.app
